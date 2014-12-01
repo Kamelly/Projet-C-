a9f088254edda6151558544e3f80ada4a3cc4a69
Kamelly <mellyb@hotmail.fr>
2014-12-01 11:08:22 +0100

    resolution conflit - a verifier (flo)

--- Enseignant.h
+++ Enseignant.h
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-#ifndef DEF_ENSEIGNANT
-#define ENSEIGNANT
-
-#include<string>
-#include<Utilisateur.h>
-=======
 #ifndef ENSEIGNANT
 #define ENSEIGNANT
 
 #include<string>
 #include"Utilisateur.h"
->>>>>>> fb3ac5603c51c645041796e602c831db4e707e9d
+
 
 
 class Enseignant
