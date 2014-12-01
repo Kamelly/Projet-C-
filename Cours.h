02b80a7e00e91b5de7c83a6d0118479d26a5e1d8
Kamelly <mellyb@hotmail.fr>
2014-12-01 10:41:41 +0100

    resoudre conflits

--- Cours.h
+++ Cours.h
@@ -24,16 +24,6 @@ class Cours
       private :
               
       std::string nom;
-<<<<<<< HEAD
-      std::time dateDebut;
-      std::time dateFin;
-      int statut; /*-1 si le cours est en attente,0 si il est validé, 1 si il est commencé */
-      Enseignant prof;
-      /* COLLECTIONS */
-      std::vector<Etudiant> lp();
-      std::vector<Etudiant> la();
-      std::vector<Ressource> lr();
-=======
       std::time_t dateDebut;
       std::time_t dateFin;
       int statut; /*-1 si le cours est en attente,0 si il est validé, 1 si il est commencé */
@@ -42,32 +32,15 @@ class Cours
       std::vector<Etudiant> lp;
       std::vector<Etudiant> la;
       std::vector<Ressource> lr;
->>>>>>> fb3ac5603c51c645041796e602c831db4e707e9d
+
       int nbMaxInscrits;
       
       
       public :
              
       Cours(); /* Constructeur*/
-<<<<<<< HEAD
-<<<<<<< HEAD
-      std::string getNom();
-=======
       ~Cours(); /*Destructeur */
-      int getNom();
->>>>>>> 723c17b0292328c2b12047611279656bd2ca4ac9
-      std::time getDateDebut();
-      std::time getDateFin();
-      int getStatut();
-      void setStatut(int statut);
-      Enseignant getProf(); 
-      vector <Etudiant> getListePrinc();
-      vector <Etudiant> getListeAttente();
-      vector <Ressource> getListeRessource();
-      int getPlace(Etudiant &e, vector<Etudiant> i);
-=======
-      ~Cours(); /*Destructeur */
-	  std::string getNom();
+	  std::string getNom();  
       std::time_t getDateDebut();
       std::time_t getDateFin();
       int getStatut();
@@ -77,17 +50,12 @@ class Cours
       std::vector<Etudiant> getListeAttente();
       std::vector<Ressource> getListeRessource();
       int getPlace(Etudiant &e, std::vector<Etudiant> i);
->>>>>>> fb3ac5603c51c645041796e602c831db4e707e9d
       int rechercheEtudiant(Etudiant &e);
       int inscrire(Etudiant &e);
       void addRessource (Ressource &r);
       Ressource getRessource(std::string nom);
       int getMaxInscrit();
-<<<<<<< HEAD
-      boolean lpPleine();   
-=======
       bool lpPleine();   
->>>>>>> fb3ac5603c51c645041796e602c831db4e707e9d
       
 };
 
