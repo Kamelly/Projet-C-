de35f00a2f61b8aef48156fac3afc8c73775a300
Kamelly <mellyb@hotmail.fr>
2014-12-01 10:58:27 +0100

    resolution conflits - a verifier (flo)

--- Etudiant.h
+++ Etudiant.h
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-#ifndef DEF_ETUDIANT
-#define ETUDIANT
-
-#include<string>
-#include<Utilisateur.h>
-=======
 #ifndef ETUDIANT
 #define ETUDIANT
 
 #include<string>
 #include "Utilisateur.h"
->>>>>>> fb3ac5603c51c645041796e602c831db4e707e9d
-
 
 class Etudiant
 {
