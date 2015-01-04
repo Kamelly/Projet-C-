41708ae57b5ca71068680258dc508e87e6d9e0f0
Florine <florine.simon54@gmail.com>
2015-01-04 21:04:19 +0100

    Modif main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,33 @@
 #include <stdio.h>
+#include <iostream>
+#include"Plateforme.h"
+
+using namespace std;
+
+long double x, y;
+string login;
 
 int main(int argc, const char* argv[])
 {
-	// Prints each argument on the command line.
-	printf("\nHello World\n\n");
+	printf("\nBienvenue sur la Plateforme de Gestion de Cours!\n");
+	cout << "Entrez votre login : ";
+	getline(cin, login);
+	cout << "Bonjour, " << login << "!\n";
+	Plateforme p = Plateforme(login);
+	if (login == "admin"){
+		printf("Vous etes l'admin! \n");
+	}
+	else{
+		string statut = login.substr(0, 1);
+		cout << statut << ;
+		if (statut == "e"){
+			// Etudiant
+		}
+		else{
+			// Enseignant
+		}
+	}
+	system("pause");
+	return 0;
 
 }
